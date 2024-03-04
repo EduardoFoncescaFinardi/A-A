008d27ed3478ac3235b018492a28e54450546f69
EduardoFoncescaFinardi <edufinardi@outlook.com>
2024-03-04 20:20:37 -0300

    first commit

---
+++ src/main/java/com/aea/aea/AeaApplication.java
@@ -0,0 +1,13 @@
+package com.aea.aea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aea/aea/AeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aea.aea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
