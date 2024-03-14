81b166a673e937472aea2b0743a7c205bee16913
EduardoFoncescaFinardi <edufinardi@outlook.com>
2024-03-14 19:53:04 -0300

    lombok

---
+++ src/main/java/com/aea/aea/repository/ServicoRepository.java
@@ -0,0 +1,10 @@
+package com.aea.aea.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aea.aea.model.Servico;
+
+public interface ServicoRepository extends JpaRepository<Servico, Long> {
+
+
+}
